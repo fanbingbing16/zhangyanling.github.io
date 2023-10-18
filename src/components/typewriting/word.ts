b92c6ad23e9857c01be014fe75819729e4e046cd
zhangyl <2581932570@qq.com>
2023-10-18 11:30:33 +0800

    delete

--- src/components/typewriting/word.ts
+++
@@ -1,6 +0,0 @@
-export const word = [
-  'style', '今天的太阳很毒', 'help', 'word', 'file', 'edit', 'main', '我不想要呆这里了', 'node', 'vue', 'hospital',
-  'random', 'projects', 'configuration', 'form', 'button', 'duiisabled', 'by','components','typewriting',
-  'default', 'primary', 'picker', 'javascript', 'less', 'typescript', 'bug', 'iconfont', 'login', 'program', 'service', 'api',
-  'angular','localhost','notes','start','pages','consol','html','web','stage'
-]
